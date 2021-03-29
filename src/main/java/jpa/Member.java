209d8cce71678f665b451a6dc1e5ffdd07487f52
yoondev93 <yoondev93@gmail.com>
2021-03-29 21:49:18 +0900

    feat: Add persistence management example code

--- src/main/java/jpa/Member.java
+++ src/main/java/jpa/Member.java
@@ -3,13 +3,17 @@ package jpa;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 //@Table(name = "USER") // Table명 변경
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
   @Id
---
+++ src/main/java/jpa/Orm02Main.java
@@ -0,0 +1,47 @@
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Orm02Main {
+
+  public static void main(String[] args) {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("basic");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+      // 비영속
+      Member member = new Member();
+      member.setId(1L);
+      member.setName("member01");
+
+      // 영속 && 쓰기 지연
+      em.persist(member);
+
+      System.out.println("=====");
+      // 1차캐시
+      Member findMember01 = em.find(Member.class, 1L);
+      Member findMember02 = em.find(Member.class, 1L);
+      System.out.println("=====");
+
+      // 동일성 보장
+      System.out.println("findeMember01 == FindMember02 :" + (findMember01 == findMember02));
+
+      // 변경 감지
+      findMember01.setName("changeMember01");
+
+      // 데이터베이스에 SQL Query 실행
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+    emf.close();
+  }
+}
